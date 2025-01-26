da866b4415a7b68d6cff3c371879fbb11b7538a0
Antonio Núñez de Santos <167419700+aanunnezz@users.noreply.github.com>
2025-01-26 20:07:58 +0100

    Add files via upload

---
+++ Bloque4/despliegueKubernetes.py
@@ -0,0 +1,30 @@
+import os
+
+def run_command(command):
+    print(f"Ejecutando: {command}")
+    result = os.system(command)
+    if result != 0:
+        print(f"Error al ejecutar: {command}")
+        exit(1)
+
+# Iniciamos un clúster de Kubernetes con 3 nodos
+run_command("minikube start --driver=docker -p pc2 --nodes 3")
+run_command("minikube profile pc2")
+
+# Aplicamos los archivos YAML para desplegar los microservicios
+run_command("minikube kubectl apply -f product-page.yaml")
+run_command("minikube kubectl apply -f details.yaml")
+run_command("minikube kubectl apply -f ratings.yaml")
+run_command("minikube kubectl apply -f reviews-svc.yaml")
+
+# Desplegamos las versiones de Reviews
+run_command("minikube kubectl apply -- -f reviews-v1.yaml")
+run_command("minikube kubectl apply -- -f reviews-v2.yaml")
+run_command("minikube kubectl apply -- -f reviews-v3.yaml")
+
+# Obtenemos el estado de los pods y servicios
+run_command("minikube kubectl get pods")
+run_command("minikube kubectl get services")
+
+# Accedemos al servicio productpage
+run_command("minikube service product-page")

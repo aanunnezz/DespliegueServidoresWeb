19c1cb0e6c725371c7651060ec8f237af5507e9b
Antonio Núñez de Santos <167419700+aanunnezz@users.noreply.github.com>
2025-02-24 14:05:42 +0100

    Update despliegueKubernetes.py

--- Bloque4/despliegueKubernetes.py
+++ Bloque4/despliegueKubernetes.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 def run_command(command):
     print(f"Ejecutando: {command}")
@@ -8,23 +9,23 @@ def run_command(command):
         exit(1)
 
 # Iniciamos un clúster de Kubernetes con 3 nodos
-run_command("minikube start --driver=docker -p pc2 --nodes 3")
-run_command("minikube profile pc2")
+run_command("minikube start")
 
 # Aplicamos los archivos YAML para desplegar los microservicios
-run_command("minikube kubectl apply -f product-page.yaml")
-run_command("minikube kubectl apply -f details.yaml")
-run_command("minikube kubectl apply -f ratings.yaml")
-run_command("minikube kubectl apply -f reviews-svc.yaml")
+run_command("minikube kubectl -- apply -f productpage.yaml")
+run_command("minikube kubectl -- apply -f details.yaml")
+run_command("minikube kubectl -- apply -f ratings.yaml")
+run_command("minikube kubectl -- apply -f reviews-svc.yaml")
+
 
 # Desplegamos las versiones de Reviews
-run_command("minikube kubectl apply -- -f reviews-v1.yaml")
-run_command("minikube kubectl apply -- -f reviews-v2.yaml")
-run_command("minikube kubectl apply -- -f reviews-v3.yaml")
+run_command("minikube kubectl -- apply  -f reviews-v1.yaml")
+run_command("minikube kubectl -- apply  -f reviews-v2.yaml")
+run_command(f"minikube kubectl -- apply -f reviews-v3.yaml")
 
 # Obtenemos el estado de los pods y servicios
-run_command("minikube kubectl get pods")
-run_command("minikube kubectl get services")
+run_command("minikube kubectl  get pods")
+run_command("minikube kubectl  get services")
 
 # Accedemos al servicio productpage
-run_command("minikube service product-page")
+run_command("minikube service productpage")
